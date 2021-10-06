29e7eb9f2bc7fcc123a98399df27e21866610f0e
flying-gwx <flying-gwx@sjtu.edu.cn>
2021-10-06 18:32:51 +0800

    test

--- QR_image.py
+++ QR_image.py
@@ -2,7 +2,7 @@ from MyQR import myqr # 需先安装MyQR库
 
 def QR_myqr():
     myqr.run(
-        'https://flying-gwx.github.io/ForYou/test.gif', # 二维码指向链接，或无格式文本（但不支持中文）
+        'https://flying-gwx.github.io/ForYou/page.html', # 二维码指向链接，或无格式文本（但不支持中文）
         version = 5, # 大小1~40
         level='H', # 纠错级别
        # picture = 'img.jpg', # 底图路径
