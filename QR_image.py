4db86de324eecd420085f6e5c663fb0c5700ca7e
flying-gwx <flying-gwx@sjtu.edu.cn>
2021-10-11 20:27:14 +0800

    add album

--- QR_image.py
+++ QR_image.py
@@ -5,11 +5,11 @@ def QR_myqr():
         'https://flying-gwx.github.io/ForYou/page.html', # 二维码指向链接，或无格式文本（但不支持中文）
         version = 5, # 大小1~40
         level='H', # 纠错级别
-       # picture = 'img.jpg', # 底图路径
+        picture = 'for_QR.jpg', # 底图路径
         colorized = True, # 彩色
         contrast = 1.0, # 对比度
         brightness = 1.0, # 亮度
-        save_name = 'save1.jpg', # 保存文件名
+        save_name = 'save1.png', # 保存文件名
         
     )
 
